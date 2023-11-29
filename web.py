0504a68df670419b3fcebe8b3f5ff6bb9c1333b2
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-11-29 06:01:20 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,12 +1,6 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
-import json
 
-def load_lottiefiles(filepath: str):
-    with open(filepath, "r") as f:
-        return json.load(f)
-
-lottie_coding = load_lottiefiles("C:\Users\Admin\Desktop\Python\code.json")
   
 st.title("The Website")
 st.header("I am Currently Learning Python")
@@ -24,13 +18,7 @@ button = st.button("Done")
 st.markdown("The Code I Used")
 st.code('''import streamlit as st
 from streamlit_lottie import st_lottie
-import json
 
-def load_lottiefiles(filepath: str):
-    with open(filepath, "r") as f:
-        return json.load(f)
-
-lottie_coding = load_lottiefiles("C:\Users\Admin\Desktop\Python\code.json")
   
 st.title("The Website")
 st.header("I am Currently Learning Python")
@@ -38,17 +26,9 @@ st.header("I am Currently Learning Python")
 name = st.text_input("Enter Your Name")
 
 classdata = st.selectbox
-        
-        
-button = st.button("Done")
-st_lottie(
-    lottie_coding,
-    loop = True,
-    quality="high")''')
+
+
 
 
 button = st.button("Done")
-st_lottie(
-    lottie_coding,
-    loop = True,
-    quality="high")
+''')
