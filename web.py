47aecb0e35211121f29a39ff8c75abd47d73107d
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-12-01 11:14:22 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,13 +1,30 @@
 import streamlit as st
-  
+import pandas as pd
+import numpy as np
+
+
 st.title("The Website")
 st.header("I am Currently Learning Python")
+df = pd.DataFrame(np.random.randn(800, 2) / [50, 50] + [19.07, 72.87],columns=['latitude', 'longitude'])
+@st.cache
 
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+csv = convert_df(df)
+st.download_button( 
+
+    label="Download data as CSV",
+
+    data=csv,
+
+    file_name='sample_df.csv',
+
+    mime='text/csv',
+
+)
 name = st.text_input("Enter Your Name")
 
 classdata = st.selectbox
-
-
 
 
 button = st.button("Done")
