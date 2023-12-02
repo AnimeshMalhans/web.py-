118e761cd907a654c397c97cd34c398d5add63f5
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-12-02 10:22:04 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,37 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from streamlit_option_menu import option_menu
 
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["Home Page", "About Me","The Code"]
+    )
 
-st.title("The Website")
-st.header("I am Currently Learning Python")
-df = pd.DataFrame(np.random.randn(800, 2) / [50, 50] + [19.07, 72.87],columns=['latitude', 'longitude'])
-@st.cache
+if selected == "Home Page":
+    st.title("The Website")
+    st.markdown("The Code I Used")
 
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
-csv = convert_df(df)
-st.download_button( 
+if selected == "About Me":
+    st.header("I am Currently Learning Python")
 
-    label="Download data as CSV",
-
-    data=csv,
-
-    file_name='sample_df.csv',
-
-    mime='text/csv',
-
-)
-name = st.text_input("Enter Your Name")
-
-classdata = st.selectbox
-
-
-button = st.button("Done")
-
-
-st.markdown("The Code I Used")
-st.code('''import streamlit as st
+if selected == "The Code":
+    st.title(f'The Code I Used For This Website')
+    st.code('''import streamlit as st
 from streamlit_lottie import st_lottie
 
   
