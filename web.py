d862cbc348fb43165ebadda88e7fcdf17ff64d82
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-12-02 10:52:48 -0800

    Add files via upload

---
+++ web.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["Home Page", "About Me","The Code"]
+    )
+
+if selected == "Home Page":
+    st.title("Hello To Everyone.Its My Website :)")
+
+if selected == "About Me":
+    st.header("I Am Currently Learning Python")
+    st.text("Its My First Language")
+
+if selected == "The Code":
+    st.title(f'The Code I Used For This Website')
+    st.code('''import streamlit as st
+from streamlit_lottie import st_lottie
+
+  
+st.title("The Website")
+st.header("I am Currently Learning Python")
+
+name = st.text_input("Enter Your Name")
+
+classdata = st.selectbox
+
+
+
+
+button = st.button("Done")
+''')
