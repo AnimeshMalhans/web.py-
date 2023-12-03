da64c01e0f3f584091c49e4ff6ffafc7c6cd04b4
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-12-03 01:15:40 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,17 @@
 import json
+
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_hello = load_lottieurl("https://lottie.host/3ae4dee3-428a-44f2-b4e7-45875d07b2c1/cYLs3qG5Kd.json")
 
 hide_st_style = """
             <style>
@@ -18,13 +29,13 @@ with st.sidebar:
         icons=["house", "book", "code-slash"],
         menu_icon="cast",
         default_index=0,
-    )    
-
+    )
 
 if selected == "Home Page":
     st.title("Hello To Everyone.Its My Website :)")
     options=["Home Page", "About Me","The Code"]
-
+    st_lottie(lottie_hello, key="hello")
+   
 
 if selected == "About Me":
     st.title("My Name Is Animesh Malhans")
