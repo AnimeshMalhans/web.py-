671e1cc43e2581421dafdd77d5c2f2665a0e4cd4
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2023-12-03 08:45:20 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,55 +1,6 @@
 import json
-
-import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
-from streamlit_option_menu import option_menu
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_hello = load_lottieurl("https://lottie.host/3ae4dee3-428a-44f2-b4e7-45875d07b2c1/cYLs3qG5Kd.json")
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        options=["Home Page", "About Me","The Code"],
-        icons=["house", "book", "code-slash"],
-        menu_icon="cast",
-        default_index=0,
-    )
-
-if selected == "Home Page":
-    st.title("Hello To Everyone.Its My Website :)")
-    options=["Home Page", "About Me","The Code"]
-    st_lottie(lottie_hello, key="hello")
-   
-
-if selected == "About Me":
-    st.title("My Name Is Animesh Malhans")
-    st.header("I Wanna Become A Game Developer")
-    st.text("I Am Currently Learning Python")
-    st.text("Its My First Language")
-    st.write("[Social >]Instagram, https://www.instagram.com/animesh_malhansh00/")
-    st.write("Github, https://github.com/AnimeshMalhans")
-    
-
-if selected == "The Code":
-    st.title(f'The Code I Used For This Website')
-    st.code('''import json
-import streamlit as st
 from streamlit_option_menu import option_menu
 
 hide_menu_style = """
@@ -60,10 +11,16 @@ hide_menu_style = """
 """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
+def load_lottiefiles(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+lottie_coding = load_lottiefiles("lottifiles/coding.json")
+
 with st.sidebar:
     selected = option_menu(
         menu_title="Main Menu",
-        options=["Home Page", "About Me","The Code"],
+        options=["Home Page", "About Me",],
         icons=["house", "book", "code-slash"],
         menu_icon="cast",
         default_index=0,
@@ -72,6 +29,10 @@ with st.sidebar:
 
 if selected == "Home Page":
     st.title("Hello To Everyone.Its My Website :)")
+    st_lottie(
+    lottie_coding,
+    loop=True,
+    quality="high"
 )
     options=["Home Page", "About Me","The Code"]
 
@@ -81,9 +42,3 @@ if selected == "About Me":
     st.header("I Wanna Become A Game Developer")
     st.text("I Am Currently Learning Python")
     st.text("Its My First Language")
-    
-
-if selected == "The Code":
-    st.title(f'The Code I Used For This Website')
-    st.code(
-''')
